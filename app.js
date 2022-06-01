d1a0f0c8e2d98c5a9e0f4026dab245fcfdca5571
MBulti <m.bulthaup.development@gmail.com>
2022-06-01 16:32:54 +0200

    added round change

--- app.js
+++ app.js
@@ -1,48 +1,71 @@
-// backend
-let currentCollection = "Round1";
-
-// real time listener
-db.collection(currentCollection).onSnapshot(snapshot => {
-    console.log(snapshot.docChanges());
-    snapshot.docChanges().forEach(change => {
-        if (change.type === "added") {
-            console.log("Add", change.doc.data());
-            renderGuess(change.doc.data().Index, change.doc.data().Value)
-        }
-        else if (change.type === "removed") {
-            console.log("Remove", change.doc.data());
-        }
-    });
-});
-
-
-// frontend
+// main
 var arrNames = [];
-
-var lastClicked;
-var grid = createGrid(10, 10, function(el,row,col,i) {
-    let calcIndex = row * 10 + col;
-    console.log("element:",el);
-    console.log("row:",row);
-    console.log("col:",col);
-
-    const guess = {
-        Index: calcIndex,
-        Value: prompt("Name")
-    }
-    db.collection(currentCollection).add(guess).catch(err => console.log(err));
-    //console.log(arrNames)
-});
+let currentCollectionNumber = 1;
+var realTimeListener = startListener();
+var grid = createGuessGrid();
 var table = createGuessTable();
 
 document.body.appendChild(grid);
 document.body.appendChild(table);
      
+
+// frontend
+$('select[id=roundselect]').change(function() {
+    selectedValue = $(this).val();
+    if (selectedValue === "")
+    {
+        // get the current round count
+        currentCollectionNumber = $('option', this).length;
+        $('<option>')
+            .text("Runde " + currentCollectionNumber)
+            .attr('value', currentCollectionNumber)
+            .insertBefore($('option[value=""]', this)); // this is only possible in JQuery... otherwise we could use good old plain JS
+        $(this).val(currentCollectionNumber);
+        changeRound(currentCollectionNumber)
+    } else {
+        changeRound(selectedValue);
+    }
+});
+function changeRound(value) {
+    // remove old
+    this.realTimeListener();
+    var grid = document.getElementById('fieldgrid');
+    grid.innerHTML = '';
+    arrNames = [];
+
+    // update and redraw the grid, due to firebase "onChange" we need to redraw the whole table ... 
+    currentCollectionNumber = value;
+    realTimeListener = startListener();
+    grid = createGuessGrid(); 
+}
+function createGuessGrid() {
+    return createGrid(10, 10, function(el,row,col,i) {
+        let calcIndex = row * 10 + col;
+        console.log("element:",el);
+        console.log("row:",row);
+        console.log("col:",col);
+    
+        let guessName = prompt("Name");
+        if (guessName) {
+    
+            const guess = {
+                Index: calcIndex,
+                Value: guessName,
+                IsWinner: false,
+            }
+            db.collection(currentCollection()).add(guess).catch(err => console.log(err));
+        } else {
+            console.log("Failed to enter");
+        }
+        //console.log(arrNames)
+    });
+}
+function createGuessTable() {
+    return document.createElement('table');
+}
 function createGrid(rowcount, colcount, callback ){
     var i=0;
-    var grid = document.createElement('table');
-    grid.id = "fieldgrid"
-    grid.className = "grid";
+    var grid = document.getElementById('fieldgrid');
     for (var r=0; r < rowcount; r++){
         var tr = grid.appendChild(document.createElement('tr'));
         for (var c=0; c < colcount; c++){
@@ -57,22 +80,42 @@ function createGrid(rowcount, colcount, callback ){
     }
     return grid;
 }
-
-function createGuessTable() {
-    return document.createElement('table');
-}
-
-function renderGuess(key, value) {
+function renderGuess(key, value, isWinner = false) {
     let cell = document.getElementById("cell" + key);
     if (cell) {
         cell.innerHTML = "X";
 
-        if (arrNames[key] != null && arrNames[key].length > 0) {
-            cell.className = "multiclicked"
-            arrNames[key].push(value);
-        } else {
-            cell.className = "clicked";
-            arrNames[key] = [value];
+        if (isWinner) {
+            cell.className = "winner";
+        }
+        else {
+            if (arrNames[key] != null && arrNames[key].length > 0) {
+                cell.className = "multiclicked"
+                arrNames[key].push(value);
+            } else {
+                cell.className = "clicked";
+                arrNames[key] = [value];
+            }
         }
     }
+}
+function currentCollection() {
+    return "Round" + currentCollectionNumber;
+}
+
+// backend
+function startListener() {
+    console.log(currentCollection())
+    return db.collection(currentCollection()).onSnapshot(snapshot => {
+        console.log(snapshot.docChanges());
+        snapshot.docChanges().forEach(change => {
+            if (change.type === "added") {
+                console.log("Add", change.doc.data());
+                renderGuess(change.doc.data().Index, change.doc.data().Value, change.doc.data().IsWinner)
+            }
+            else if (change.type === "removed") {
+                console.log("Remove", change.doc.data());
+            }
+        });
+    });
 }
